d8c92faaedfb0f294fbe61e1e1b7e1fa360f05e7
C0 <103413294+SY-ML@users.noreply.github.com>
2023-03-18 17:48:43 +0900

    031823

--- Human Traffic of Stadium.sql
+++ Human Traffic of Stadium.sql
@@ -1,10 +1,18 @@
--- Not completed. TBC...
+-- https://learn-and-run.tistory.com/188
 WITH std AS (
     SELECT 
         id, visit_date, people, id - ROW_NUMBER() OVER(ORDER BY id) AS row_num
     FROM stadium
     WHERE people >= 100
+),
+
+std_cons AS (
+    SELECT row_num
+    FROM std
+    GROUP BY row_num
+    HAVING COUNT(id) >= 3
 )
 
-SELECT * FROM std
-
+SELECT id, visit_date, people
+FROM std
+WHERE row_num IN (SELECT row_num FROM std_cons)
